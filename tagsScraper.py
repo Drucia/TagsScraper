07b6c939b3dc7be38f153d5b8562d6d46616f323
Druciak <ola.druciak@gmail.com>
2021-03-23 13:32:04 +0100

    add write to csv

--- tagsScraper.py
+++ tagsScraper.py
@@ -3,8 +3,10 @@ import pandas as pd
 from tqdm import tqdm
 import requests
 import json
-
-timeout = 10  # in seconds
+import os
+import requests
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
 
 class bcolors:
     DEFAULT = "\033[39m"
@@ -19,15 +21,50 @@ class bcolors:
     UNDERLINE = '\033[4m'
 
 class tags:
-    EDUCATION = ["school", "university"]
-    HEALTH = ["hospital", "pharmacy"]
+    EDUCATION = ["school", "university", "language_school"]
+    HEALTH = ["hospital"]
     TRANSPORT = ["bus_station"]
     SHOPPING_MALL = ["mall", "cinema"]
 
-def scrap_tags_from(json_resp):
+def scrap_tag_from(json_resp):
     nodes = json_resp['elements']
-    for node in nodes:
+    all_tags = list(map(scrap_tag, nodes))
 
+    tags_counter = {
+        "education" : count_tags(tags.EDUCATION, all_tags),
+        "health": count_tags(tags.HEALTH, all_tags),
+        "transport": count_tags(tags.TRANSPORT, all_tags),
+        "shopping": count_tags(tags.SHOPPING_MALL, all_tags)
+    }
+
+    best = max(tags_counter, key=tags_counter.get)
+    return "other" if tags_counter[best] == 0 else best
+
+def count_tags(tag_names, tags_to_check):
+    return sum([tags_to_check.count(tag) for tag in tag_names])
+
+def scrap_tag(node):
+    tags_of_node = node['tags']
+    return tags_of_node['amenity'] if 'amenity' in tags_of_node else tags_of_node['shop']
+
+def requests_retry_session(
+    retries=5,
+    backoff_factor=0.3,
+    status_forcelist=(500, 502, 504),
+    session=None,
+):
+    session = session or requests.Session()
+    retry = Retry(
+        total=retries,
+        read=retries,
+        connect=retries,
+        backoff_factor=backoff_factor,
+        status_forcelist=status_forcelist,
+    )
+    adapter = HTTPAdapter(max_retries=retry)
+    session.mount('http://', adapter)
+    session.mount('https://', adapter)
+    return session
 
 def main():
     if (len(sys.argv) < 3):
@@ -37,22 +74,49 @@ def main():
 
     inputFile = sys.argv[1]
     outFile = sys.argv[2]
-    around = sys.argv[3] if (len(sys.argv) == 4) else 500
+    around = sys.argv[3] if (len(sys.argv) == 4) else 300
+    tmpFile = "tmp.csv"
 
-    coordinatesDf = pd.read_csv(inputFile)
-    for index, row in tqdm(coordinatesDf.iterrows()):
-        print(
-            f'\n🔽{bcolors.OKBLUE} Tags number: {index + 1}...')
+    coordinates_df = pd.read_csv(inputFile)
+    tags_results = []
+
+    if not os.path.exists(tmpFile):
+        tmp = pd.DataFrame([], columns=["number", "tag", "request"])
+        tmp.to_csv(tmpFile, index=False)
+
+    data_shape = coordinates_df.shape[0]
+    for index, row in tqdm(coordinates_df.iterrows(), total=data_shape):
+        print(f'\n🔽{bcolors.OKBLUE} Tag number: {index + 1} out of {data_shape} ...')
 
         request_url = f"""https://overpass-api.de/api/interpreter?data=[out:json]; 
                         (
-                            node['amenity'~'school|university|hospital|pharmacy|bus_station'|'cinema'](around:{around}, {row['lat']}, {row['lng']});
+                            node['amenity'~'school|university|hospital|bus_station|cinema'](around:{around}, {row['lat']}, {row['lng']});
                             node['shop'~'mall'](around:{around}, {row['lat']}, {row['lng']});
                         );
                         out;"""
-        response = requests.get(request_url, timeout=timeout)
-        json_resp = json.loads(response.text)
-        scrap_tags_from(json_resp)
-        return
+        response = requests_retry_session().get(request_url)
+
+        if (response.status_code != 200):
+            print(f'{bcolors.FAIL}❌ Request failed for tag number: {row["number"]}')
+
+            to_add = [row['number'], "unknown", request_url]
+            tmp = pd.DataFrame([to_add], columns=["number", "tag", "request"])
+            tmp.to_csv(tmpFile, mode='a', header=False, index=False)
+        else:
+            json_resp = json.loads(response.text)
+            tag_for_node = scrap_tag_from(json_resp)
+
+            to_add = [row['number'], tag_for_node]
+            tags_results.append(to_add)
+
+            to_add.append("")
+            tmp = pd.DataFrame([to_add], columns=["number", "tag", "request"])
+            tmp.to_csv(tmpFile, mode='a', header=False, index=False)
+        print(bcolors.OKGREEN)
+
+    tags_df = pd.DataFrame(tags_results, columns=["number", "tag"])
+    tags_df.to_csv(outFile, index=False)
+    print(f'\n🔽{bcolors.OKGREEN} All tags: {len(tags_results)} was save successfully to: {outFile}')
+    print(bcolors.DEFAULT)
 
 main()
